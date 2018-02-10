5f0cd356c75ba2f8db6e67566feb97b6e685acbe
Braňo Pilňan <B.Pilnan@gmail.com>
2018-02-10 12:41:39 +0000

    Remove `using namespace ...`
    Don't do this. Prefer having `std::` everywhere.

--- demo-kalman.cpp
+++ demo-kalman.cpp
@@ -1,11 +1,10 @@
 #include "kalman.hpp"
 #include <iostream>
-using namespace std;
 
 int main()
 {
   Kalman<float> kalman(0,1,1);
   for(int i=0;i<100;i++)
-    cout << kalman.filter(i) << '\t';
-  cout << '\n';
+    std::cout << kalman.filter(i) << '\t';
+  std::cout << '\n';
 }
